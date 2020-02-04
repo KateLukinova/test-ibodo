720b1554fd6cfc7a74fc0043e281b334d92a77fd
Екатерина Лукинова <katyushta@rambler.ru>
2020-02-04 20:52:47 +0300

    two commit

---
+++ app/js/common.js
@@ -0,0 +1,24 @@
+$(function() {
+
+	$(document).ready(function() {
+
+		$('.cover-slider').each(function(){
+			var $slides = $(this).find('.cover-slider__slide');
+			var numSlides = $slides.length - 1;
+			var i = 0;
+
+			var rotate = function(){
+				$slides.removeClass('active inactive');
+				$slides.eq(i).addClass('inactive');
+
+				if(i == numSlides){
+					i = -1;
+				}
+				$slides.eq(++i).addClass('active');
+				var timer = window.setTimeout(rotate, 8000);
+			};
+			rotate();
+		});
+	});
+
+});
